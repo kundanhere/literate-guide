9617978a837d8ca2aac8673cbf0159105b401480
Kundanhere <kundankumar8377@gmail.com>
2024-05-01 13:10:50 +0530

    [feat]: Add ScrollAnimation on 'showcase heading'

---
+++ src/slices/Showcase/AnimatedContent.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function AnimatedContent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const container = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  useGSAP(
+    () => {
+      // check is prefersReducedMotion is enabled in client's browser
+      if (prefersReducedMotion) {
+        // set animation instantly and return
+        gsap.set(container.current, { y: 0 });
+        return;
+      }
+
+      gsap.fromTo(
+        container.current,
+        { y: 100 },
+        {
+          y: 0,
+          ease: "power2.inOut",
+          duration: 1,
+          scrollTrigger: {
+            trigger: container.current,
+            start: "top bottom-=40%",
+            toggleActions: "play pause resume reverse",
+          },
+        },
+      );
+    },
+    { scope: container },
+  );
+  return <div ref={container}>{children}</div>;
+}
